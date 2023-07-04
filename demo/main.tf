081e731be071ed4e49421fc6bf1487f7aef379ff
nagababu <nag4c1@gmail.com>
2023-07-04 17:16:57 +0530

    frontend code

--- demo/main.tf
+++ demo/main.tf
@@ -1,8 +1,31 @@
 resource "aws_instance" "sample_ec2" {
   ami = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name = "sample ec2 instance"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
   }
 }
