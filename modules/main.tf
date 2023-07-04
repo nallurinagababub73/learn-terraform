2ef0aa83cd576d82f6b2a4b4676724e413f655ae
nagababu <nag4c1@gmail.com>
2023-07-04 19:27:11 +0530

    frontend code

--- modules/main.tf
+++ modules/main.tf
@@ -10,6 +10,6 @@ module "catalogue" {
 output "public_ip_frontend" {
   value = module.frontend.public_ip
 }
-output "public_ip" {
+output "public_ip_catalogue" {
   value = module.catalogue.public_ip
 }
