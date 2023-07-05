90272d453a35e73082a686d4718169cbbc7756c3
nagababu <nag4c1@gmail.com>
2023-07-05 18:50:50 +0530

    frontend code

--- statefile/state.tf
+++ statefile/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "4338"
-    key    = "naga/terraform.tfstate"
+    bucket = "naga-b73"
+    key    = "hema/terraform.tfstate"
     region = "us-east-1"
   }
 }
