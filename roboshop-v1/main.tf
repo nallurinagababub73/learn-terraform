60e54a81805654f6cca488827331ecdb6a64f050
nagababu <nag4c1@gmail.com>
2023-07-05 18:05:24 +0530

    frontend code

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -2,42 +2,42 @@ module "frontend" {
   source = "./ec2"
   name = "frontend"
 }
-module "catalogue" {
-  source = "./ec2"
-  name = "catalogue"
-}
-module "user" {
-  source = "./ec2"
-  name = "user"
-}
-module "cart" {
-  source = "./ec2"
-  name = "cart"
-}
-module "shipping" {
-  source = "./ec2"
-  name = "shipping"
-}
-module "payment" {
-  source = "./ec2"
-  name = "payment"
-}
-module "mongodb" {
-  source = "./ec2"
-  name = "mongodb"
-}
-module "mysql" {
-  source = "./ec2"
-  name = "mysql"
-}
-module "redis" {
-  source = "./ec2"
-  name = "redis"
-}
-module "rabbitmq" {
-  source = "./ec2"
-  name = "rabbitmq"
-}
+#module "catalogue" {
+#  source = "./ec2"
+#  name = "catalogue"
+#}
+#module "user" {
+#  source = "./ec2"
+#  name = "user"
+#}
+#module "cart" {
+#  source = "./ec2"
+#  name = "cart"
+#}
+#module "shipping" {
+#  source = "./ec2"
+#  name = "shipping"
+#}
+#module "payment" {
+#  source = "./ec2"
+#  name = "payment"
+#}
+#module "mongodb" {
+#  source = "./ec2"
+#  name = "mongodb"
+#}
+#module "mysql" {
+#  source = "./ec2"
+#  name = "mysql"
+#}
+#module "redis" {
+#  source = "./ec2"
+#  name = "redis"
+#}
+#module "rabbitmq" {
+#  source = "./ec2"
+#  name = "rabbitmq"
+#}
 output "frontend_dns" {
   value = module.frontend.dns-record
 }
