e63fb189c39075450cd6796701e083b3be4c21cb
nagababu <nag4c1@gmail.com>
2023-07-05 22:07:45 +0530

    frontend code

--- functions/lookup/main.tf
+++ functions/lookup/main.tf
@@ -5,3 +5,34 @@ variable "components" {
 output "component_name" {
   value = element(var.components,7 )
 }
+variable "roboshop" {
+  default = {
+    presentation = {
+      frontend1 = "nginx"
+      frontend2 = "appache"
+    }
+    application = {
+      app1 = "catalogue"
+      app2 = "cart"
+    }
+    database = {
+      db1 = "mongodb"
+      db2 = "redis"
+      db3 = "mysql"
+      db4 = {
+        dispatch = "rabbitmq"
+      }
+
+    }
+  }
+}
+
+output "layer1" {
+  value = var.roboshop["presentation"]
+}
+output "layer2" {
+  value = var.roboshop["appilication"]["app1"]
+}
+output "layer3" {
+  value = var.roboshop["database"]["db4"]["dispatch"]
+}
