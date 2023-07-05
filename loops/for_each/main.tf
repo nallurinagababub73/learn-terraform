133fa7aed5d63d3797deb85ed4f98c4395f23d45
nagababu <nag4c1@gmail.com>
2023-07-05 22:57:32 +0530

    frontend code

---
+++ loops/for_each/main.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "ec2" {
+  for_each = var.components
+  ami = data.aws_ami.example.id
+  instance_type = each.value["instance_type"]
+
+  tags = {
+    Name = each.key
+  }
+}
+
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+
+}
+variable "components" {
+        default = {
+          frontend = {
+            name = "frontend"
+            instance_type = "t3.micro"
+          }
+          catalogue = {
+            name = "catalogue"
+            instance_type = "t3.small"
+          }
+          user = {
+            name = "user"
+            instance_type = "t3.nano"
+          }
+        }
+}
