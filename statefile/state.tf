19f2efc8f6c8a0b2873c19b7245f71cb8ed12a92
nagababu <nag4c1@gmail.com>
2023-07-05 18:39:40 +0530

    frontend code

--- statefile/state.tf
+++ statefile/state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my sample s3 bucket"
+    bucket = "my-sample-s3-bucket"
     key    = "naga/hema/pardhu/terraform.tfstate"
     region = "us-east-1"
   }
