c05685b7adb3800a4d1755df937920a965e969e5
nagababu <nag4c1@gmail.com>
2023-08-24 21:20:42 +0530

    frontend code

--- mod/ec2/main.tf
+++ mod/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "EC2" {
   count = length(var.components)
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
-  vpc_security_group_ids = var.security_group_id
+  vpc_security_group_ids = [var.security_group_id]
 
   tags = {
     Name = var.components[count.index]
